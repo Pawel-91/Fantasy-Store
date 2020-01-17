7375f420dbc5146b7dd55331c494ed0cffac8e50
Pawel-91 <pawel.szarzynski.91@gmail.com>
2020-01-17 18:56:57 +0100

    Fantasy Store - admin controller and admin index view

---
+++ FantasyStore/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using FantasyStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FantasyStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Products);
+
+    }
+}
---
+++ FantasyStoreTests/ControllerTests/AdminsControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using FantasyStore.Controllers;
+using FantasyStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace FantasyStoreTests.ControllerTests
+{
+    public class AdminsControllerTests
+    {
+        [Fact]
+        public void Index_Contains_All_Products()
+        {
+            // Arrange - creating repository mock
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3"}
+            }.AsQueryable());
+
+            // Arrange - creating controller
+            AdminController target = new AdminController(mock.Object);
+
+            // Act
+            Product[] result = GetViewModel<IEnumerable<Product>>(target.Index())?.ToArray();
+
+            // Assert
+            Assert.Equal(3, result.Length);
+            Assert.Equal("P1", result[0]?.Name);
+            Assert.Equal("P2", result[1]?.Name);
+            Assert.Equal("P3", result[2]?.Name);
+        }
+
+        private T GetViewModel<T>(IActionResult result) where T : class
+         => (result as ViewResult)?.ViewData.Model as T;
+    }
+}

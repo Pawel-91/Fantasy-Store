37b4f3a81ac0677cf5b6d7cbced97efd0a1ef4e1
Pawel-91 <pawel.szarzynski.91@gmail.com>
2020-01-22 09:24:39 +0100

    Fantasy Store - showing confirmation message and validation errors

--- FantasyStore/Models/Product.cs
+++ FantasyStore/Models/Product.cs
@@ -1,11 +1,21 @@
-﻿namespace FantasyStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FantasyStore.Models
 {
     public class Product
     {
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Add product name please.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Add description please.")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue,ErrorMessage = "Add positive price please.")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Add category please.")]
         public string Category { get; set; }
     }
 }
